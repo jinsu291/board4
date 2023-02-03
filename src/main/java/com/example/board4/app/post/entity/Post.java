f2db70b9d05f01039fa5bb60113aa8dd696a8ecb
jinsu291 <wlstn291@naver.com>
2023-02-03 18:15:11 +0900

    make Post, Appconfig

---
+++ src/main/java/com/example/board4/app/post/entity/Post.java
@@ -0,0 +1,25 @@
+package com.example.board4.app.post.entity;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Post {
+    private long id;
+    private String subject;
+    private String content;
+    private String contentHtml;
+
+    private long author;
+
+    public Post(String subject, String content, String contentHtml) {
+        this.subject = subject;
+        this.content = content;
+        this.contentHtml = contentHtml;
+    }
+
+}
